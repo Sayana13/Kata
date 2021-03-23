245e05c12d554e6030a4d2564d02aefe87d62f2d
Sayana13 <domshoevas@gmail.com>
2021-03-22 22:06:19 -0700

    Chuck Norris VII - True or False? (Beginner)

--- src/App.js
+++ src/App.js
@@ -31,6 +31,18 @@ function App(n) {
       {/*    return false*/}
       {/*}*/}
       {/*}*/}
+      <h3>Chuck Norris VII - True or False? (Beginner)</h3>
+        <p>It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+          It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+          Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+          Go show some truth who's boss!</p>
+      {/*  function ifChuckSaysSo(){*/}
+      {/*  return !true;*/}
+      {/*}*/}
+
 
       </div>
 
