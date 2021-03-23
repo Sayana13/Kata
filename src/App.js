b304569fe131c7cfb59989be01b449b959d8e51a
Sayana13 <domshoevas@gmail.com>
2021-03-22 22:53:01 -0700

    Is he gonna survive?

--- src/App.js
+++ src/App.js
@@ -73,6 +73,17 @@ function App(n) {
       {/*  if(a == b ) return true;*/}
       {/*  return false;*/}
       {/*}*/}
+
+      <h3>Is he gonna survive? JavaScript</h3>
+        <p>A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+          Return True if yes, False otherwise :)
+
+          function hero(bullets, dragons){
+            if (bullets >= dragons * 2) return true;
+            return false;
+          }
+        </p>
       </div>
 
     </div>
